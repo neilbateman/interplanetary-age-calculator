b76a3288953bed2924c4658413164072eb64414a
Neil Bateman <neilbatman@gmail.com>
2019-05-31 13:48:40 -0700

    add earthDeath method and corresponding test

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -4,25 +4,31 @@ describe('SpaceAge', function(){
 
   it('should return number of years', function(){
     const age = new SpaceAge("1992-12-20")
-    expect(age.earthAge).toEqual(26.444);
+    expect(age.earthAge).toEqual(26);
   });
 
   it('should convert earth years to mercury years', function(){
     const age = new SpaceAge("1992-12-20")
-    expect(age.mercuryAge()).toEqual(109.73);
+    expect(age.mercuryAge()).toEqual(110);
   });
 
   it('should convert earth years to venus years', function(){
     const age = new SpaceAge("1992-12-20")
-    expect(age.venusAge()).toEqual(42.998);
+    expect(age.venusAge()).toEqual(42);
   });
 
   it('should convert earth years to mars years', function(){
     const age = new SpaceAge("1992-12-20")
-    expect(age.marsAge()).toEqual(14.058);
+    expect(age.marsAge()).toEqual(14);
   });
+
   it('should convert earth years to jupiter years', function(){
     const age = new SpaceAge("1992-12-20")
-    expect(age.jupiterAge()).toEqual(2.2297);
+    expect(age.jupiterAge()).toEqual(2.2);
+  });
+
+  it('should calculate remaining years on earth', function(){
+    const age = new SpaceAge("1992-12-20")
+    expect(age.earthDeath()).toEqual(74);
   });
 });
--- src/spaceage.js
+++ src/spaceage.js
@@ -1,23 +1,27 @@
 export class SpaceAge {
   constructor(age) {
     this.age = age;
-    this.earthAge = (((Date.now() - (new Date(age).getTime())) / 31557600000).toPrecision(5) * 1);
-    this.relativeAge = [["Mercury",0.241],["Venus",0.615],["Mars",1.881],["Jupiter",11.86]];
+    this.earthAge = (((Date.now() - (new Date(age).getTime())) / 31557600000).toPrecision(2) * 1);
+    this.relativeAge = [["Mercury",0.24],["Venus",0.62],["Mars",1.88],["Jupiter",11.86]];
   }
 
   mercuryAge(){
-    return ((this.earthAge/.241)).toPrecision(5) * 1;
+    return ((this.earthAge/.24)).toPrecision(2) * 1;
   }
 
   venusAge(){
-    return ((this.earthAge/.615)).toPrecision(5) * 1;
+    return ((this.earthAge/.62)).toPrecision(2) * 1;
   }
 
   marsAge(){
-    return ((this.earthAge/1.881)).toPrecision(5) * 1;
+    return ((this.earthAge/1.88)).toPrecision(2) * 1;
   }
 
   jupiterAge(){
-    return ((this.earthAge/11.86)).toPrecision(5) * 1;
+    return ((this.earthAge/11.86)).toPrecision(2) * 1;
+  }
+
+  earthDeath(){
+    return (100 - this.earthAge).toPrecision(2) * 1;
   }
 }
